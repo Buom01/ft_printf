f3d26650891d5f1d98eecc605cdd73e0aaec60d9
Bastien ADAM <badam@e1r11p11.42.fr>
2020-02-14 02:32:41 +0100

    Draft

--- libftprintf.h
+++ libftprintf.h
@@ -6,15 +6,26 @@
 /*   By: badam <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/15 22:40:32 by badam             #+#    #+#             */
-/*   Updated: 2020/02/04 20:14:57 by badam            ###   ########.fr       */
+/*   Updated: 2020/02/14 00:42:20 by badam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFTPRINTF_H
 # define LIBFTPRINTF_H
 
+#define MAX_FLAG 16
+
+typedef struct	s_conv
+{
+	char	type;
+	char	flag[MAX_FLAG];
+}				t_conv;
+
 int				ft_printf(const char *format, ...);
 int				ft_print_int(int n);
 int				ft_print_uint(unsigned int n);
+
+int				freeup(t_list **print_sgmt, va_list *ap);
+void			print(void *s);
 
 #endif
