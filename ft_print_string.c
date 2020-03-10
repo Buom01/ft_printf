28f310dd669526c859c76bde07e7613c168b4c04
Bastien ADAM <badam@e1r6p1.42.fr>
2020-03-11 00:25:34 +0100

    Simplify

--- ft_print_string.c
+++ ft_print_string.c
@@ -6,7 +6,7 @@
 /*   By: badam <badam@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/01 01:23:32 by badam             #+#    #+#             */
-/*   Updated: 2020/03/01 03:27:52 by badam            ###   ########.fr       */
+/*   Updated: 2020/03/11 00:25:18 by badam            ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -14,16 +14,9 @@
 
 char		*print_string(va_list ap)
 {
-	char	*sgmt;
 	char	*str;
-	size_t	len;
 
 	if (!(str = va_arg(ap, char*)))
 		str = "(null)";
-	len = ft_strlen(str); 
-	if (!(sgmt = malloc((len + 1) * sizeof(char))))
-		return (NULL);
-	ft_memcpy((void*)sgmt, (void*)str, len);
-	sgmt[len] = '\0';
-	return (sgmt);
+	return (ft_strdup(str));
 }
