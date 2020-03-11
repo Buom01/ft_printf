e2683d410079f7a04d9d192c9ef7c792ec419148
Bastien ADAM <badam@e1r6p1.42.fr>
2020-03-11 03:12:21 +0100

    Parse flag zero before digit

--- ft_printf_parsing.c
+++ ft_printf_parsing.c
@@ -6,7 +6,7 @@
 /*   By: badam <badam@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/20 05:16:17 by badam             #+#    #+#             */
-/*   Updated: 2020/03/11 01:05:48 by badam            ###   ########.fr       */
+/*   Updated: 2020/03/11 03:09:28 by badam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 static int	parse_pad(char **str)
 {
-	int	n;
+	size_t	n;
 
 	(*str)++;
 	n = 0;
@@ -60,10 +60,10 @@ char		parse_flag(t_flags *flags, char **str, char c, va_list *ap)
 		return (is_converter(c));
 	if (c == '-')
 		flags->left_pad = parse_pad(str);
-	else if (ft_isdigit(c) && (*str)--)
-		flags->right_pad = parse_pad(str);
 	else if (c == '0')
 		flags->zero_pad = parse_pad(str);
+	else if (ft_isdigit(c) && (*str)--)
+		flags->right_pad = parse_pad(str);
 	else if (c == '.')
 	{
 		flags->precision = parse_precision(str, ap);
@@ -91,5 +91,5 @@ char		*convert(t_flags flags, va_list ap)
 		return (print_uhexint(flags, ap));
 	else if (flags.conv == 'X')
 		return (print_uhexint_upcase(flags, ap));
-	return (print_percent());
+	return (print_percent(flags));
 }
